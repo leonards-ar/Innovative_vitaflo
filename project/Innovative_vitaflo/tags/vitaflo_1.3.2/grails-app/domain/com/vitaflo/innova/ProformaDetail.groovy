e310d6898d8488c0841b5a066e4dd3e6e18252d5
federico <federico@4ac4832c-dd6c-11dc-94f1-d760ba12e90c>
2011-03-14 04:15:25 +0000

    Creating new release to be deployed

---
+++ project/Innovative_vitaflo/tags/vitaflo_1.3.2/grails-app/domain/com/vitaflo/innova/ProformaDetail.groovy
@@ -0,0 +1,55 @@
+package com.vitaflo.innova
+
+class ProformaDetail {
+
+    Integer quantity = 0
+    String lot
+    Double dailyDose
+    String doseUnit
+    Product product
+    Double price
+
+    static belongsTo = [proforma: Proforma]
+
+    static final def UNIT_LIST = ['mg', 'ml']
+
+    static constraints = {
+        quantity(nullable:false, min:1)
+        product(nullable:false)
+        lot(nullable:true)
+        dailyDose(nullable:true)
+        doseUnit(nullable:true, inList:UNIT_LIST)
+        price(nullable:false, min:0d)
+    }
+
+
+
+    static mapping = {
+        table 'proformadetails'
+        price column:'price_each'
+    }
+
+    static transients = ['total','productName','productPrice', 'totalDoseDays']
+
+    Integer getTotalDoseDays() {
+        if(this.product?.presentation != null && this.dailyDose != null && this.quantity != null) {
+            return Math.ceil(this.product?.presentation * this.quantity / this.dailyDose);
+        } else {
+            return new Integer(0);
+        }
+
+    }
+
+    Double getTotal(){
+        this.price * this.quantity
+    }
+
+    String getProductName(){
+        this.product.name;
+    }
+
+    String toString() {
+        return "${this.proforma} - ${this.product}"
+    }
+
+}
