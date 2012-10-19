b28f3df5a0974785d7b9c49b0c34ffbecfb256a4
federico <federico@4ac4832c-dd6c-11dc-94f1-d760ba12e90c>
2012-10-19 05:08:41 +0000

    Merged revisions 2094 via svnmerge from https://mindpool.sourcerepo.com/mindpool/repo/project/Innovative_innova/trunk
    ........
      r2094 | federico | 2011-07-08 01:00:47 -0300 (vie, 08 jul 2011) | 1 line
      
      Added pathology ABM to innova
    ........

---
+++ project/Innovative_vitaflo/trunk/grails-app/domain/com/vitaflo/innova/Pathology.groovy
@@ -0,0 +1,21 @@
+package com.vitaflo.innova
+
+class Pathology {
+	
+	String name
+	Double minDose
+	Double maxDose
+
+	static mapping = {
+		table 'pathologies'
+	}
+
+    static constraints = {
+		name(blank:false, nullable:false)
+		minDose(nullable: false, min:new Double('0.01'))
+		maxDose(nuulable: false, validator: {val, obj -> val >= obj.minDose})
+		
+    }
+	
+	
+}
